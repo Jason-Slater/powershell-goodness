9ecad74b45d11cb005b7505915f884d736095746
Jason-Slater <jasonlslater@gmail.com>
2017-03-22 20:43:27 -0500

    VSSADMIN File Change
    Script pulls servers from AD then finds a file <vssadmin> then renames
    it correctly.
    This is because a script was used to rename vssadmin due to a virus.

---
+++ vssadmin_file_change.ps1
@@ -0,0 +1,76 @@
+﻿    function Rename-FilesMatchingPattern
+
+{
+
+    param(
+
+
+        [string]$UNCPath,
+
+        [string]$filePattern,
+
+        [string]$newName,
+
+        [switch]$apply
+
+    )
+
+    $ScanPath = Join-Path -Path $UNCPath -ChildPath $filePattern
+
+    $dirFiles = get-childitem  $ScanPath
+
+    if ($dirFiles.Count -le 1)
+
+    {
+
+        foreach ($file in $dirFiles)
+
+        {
+
+            $Destination = Join-Path -Path $UNCPath -ChildPath $newName
+
+            If ($apply)
+
+            {
+
+                Move-Item -Path $file.FullName -Destination $Destination -Force
+
+                Get-item $destination
+
+            }
+
+            else 
+
+            {
+
+                $file
+
+            }
+
+        }
+
+    }
+
+    else
+
+    {
+
+        Write-Host  "Multiple Files matching pattern found."
+
+    }
+
+}
+
+# $hosts = Get-ADComputer -filter {dnshostname -like "N9adm02.neptune9.com"}
+
+$hosts = Get-ADComputer -filter {OperatingSystem -like "Windows Server*"}
+
+Foreach ($item in $hosts)
+
+{
+
+    $item.dnshostname
+
+     Rename-FilesMatchingPattern  -UNCPath "\\$($item.dnshostname)\C$\windows\system32" -filePattern  "vssadmin.exe*" -newName  "vssadmin.exe" -apply
+
+} 
