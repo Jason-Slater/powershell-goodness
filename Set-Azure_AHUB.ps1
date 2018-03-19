553939d13e4eec706744adfcb4143fa88b2bbec7
Jason Slater <jasonlslater@gmail.com>
2018-03-19 12:53:42 -0500

    Add files via upload

---
+++ Set-Azure_AHUB.ps1
@@ -0,0 +1,32 @@
+﻿<#
+.SYNOPSIS
+     RunBook Enforces AHUB Licensing on Windows Servers 
+.DESCRIPTION
+     RunBook Checks for Azure Hybrid Benifit Licencing set on Windows Server VMs, if not set then it it will be set
+.NOTES
+     Author     : Jason
+ 
+#>
+
+
+$AzureSub = @("Visual Studio Enterprise","Visual Studio Professional with MSDN")
+
+$AzureSub | ForEach-Object { 
+    Select-AzureRmSubscription $_
+        Get-AzureRMVM | ForEach-Object {
+            if ($_.StorageProfile.OSDisk.OSType -match "Windows") {
+                $_.LicenseType
+                    if ($_.LicenseType -notmatch "Windows_Server") {
+                    $_.Name
+                    $_.ResourceGroupName
+                    $_.LicenseType = "Windows_Server"
+                        Update-AzureRmVM -ResourceGroupName $_.ResourceGroupName -VM $_
+                          
+            }
+
+        }
+           
+    }
+}
+
+
