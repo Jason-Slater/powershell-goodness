bfe0a5969921d954ca1c20f2756480d2793282c3
Jason Slater <jasonlslater@gmail.com>
2019-01-06 18:23:16 -0600

    Updated script and performed some function testing

--- Patch-WindowsServers.ps1
+++ Patch-WindowsServers.ps1
@@ -18,33 +18,76 @@
  Powershell script to execute Windows Update across all servers in a domain, leveraging the PSWindowsUpdate module from PowerShell Gallery 
 
 #> 
-#Param()
-
 #Variables
-# $ADDS     = (Get-ADDomain).name
-$ModVer   = "2.1.0.1"
-$PkgVer   = "2.8.5.201"
-$CompList = Get-ADComputer -Filter { OperatingSystem -Like '*Server*' } -Properties *
 
-
-# Check PowerShell Version
-function PSVersionCheck {
-    Invoke-Command -ComputerName $Comp -ScriptBlock {$PSVersionTable.PSVersion.Major} 
-}
+# Obtain a server list
+$CompList = Get-ADComputer -Filter { OperatingSystem -Like '*Server*' } -Properties name | Select-object name
 
 # Script Body
+
 foreach ($Comp in $CompList) {
-    if ( PSVersionCheck -ge 5 ) {
-        Install-PackageProvider -Name NuGet -Minimumversion $PkgVer -Force
-        Install-Module -Name PSWindowsUpdate -MinimumVersion $ModVer -Force
-        Start-Sleep -Seconds 60
-        Install-WindowsUpdate -ComputerName $Comp -IgnoreUserInput -AcceptAll -Download -Install -AutoReboot -Verbose
 
-    }
-    else {
-        Write-Output "$Comp needs to have PSVersion Updated"
+    $Comp
+
+    $PSTestResults = (Test-NetConnection -ComputerName $Comp -CommonTCPPort WINRM).TcpTestSucceeded 
+
+    if ($PSTestResults -eq $False) {
         
-    }
-}
+        Write-Output "$Comp needs to have WINRM opened and/or configured"
+    
+    } else {
+            
+        Enter-pssession -ComputerName $Comp
 
+        start-sleep -Seconds 5
 
+        $PSVersion = ($PSVersionTable).PSVersion.Major
+
+        Write-Output "$Comp has PowerShell version $PSVersion"
+
+        Exit-PSSession
+
+        }
+
+            if ($PSVersion -lt 5 ) {
+
+                Write-Output "$Comp needs to have PowerShell Updated"
+
+            } else {
+
+                 Enter-pssession -ComputerName $Comp
+
+                 Start-sleep -Seconds 5
+
+                 $PkgVerCheck = (get-packageprovider Nuget).version.major 
+
+                 Write-Output "$Comp has NuGet major version $PkgVerCheck"
+                
+                 if ($PkgVerCheck -ne 2) {
+                    
+                    Write-Output "$Comp needs to have NuGet Package Updated"
+
+                    Exit-PSSession
+
+                 } else {
+
+                    Install-PackageProvider -Name NuGet 
+
+                    Start-Sleep -Seconds 60
+
+                    Install-Module -Name PSWindowsUpdate
+
+                    Start-Sleep -Seconds 60
+
+                    Exit-PSSession
+
+                    # Install-WindowsUpdate -IgnoreUserInput -AcceptAll -Download -Install -AutoReboot -Verbose
+                        
+                   Install-WindowsUpdate -ComputerName $Comp -IgnoreUserInput -AcceptAll -Download -Install -AutoReboot -Verbose
+
+                               
+                 }
+
+            }
+
+ }
