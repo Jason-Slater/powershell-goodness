5aa0369a912809885044961de636325bd758c0b4
Jason Slater <jasonlslater@gmail.com>
2020-01-27 20:25:23 -0600

    Removed spaces

--- Get-LastBootTime.ps1
+++ Get-LastBootTime.ps1
@@ -1,15 +1,12 @@
  $Computers = Get-ADComputer -Filter { OperatingSystem -Like '*Windows Server*'}
-
  
-foreach ($computer in $Computers) {
-    
+foreach ($computer in $Computers) {   
     If((Test-netconnection -ComputerName $computer.name -CommonTCPPort RDP -ErrorAction SilentlyContinue -WarningAction SilentlyContinue).TcpTestSucceeded -eq $False) {
            Write-Host "$computer is no bueno" -ForegroundColor Yellow   
     } ELSE {
              (Get-WmiObject -computername $computer.name win32_operatingsystem |
                 select-object csname, @{LABEL='LastBootUpTime';EXPRESSION={$_.ConverttoDateTime($_.lastbootuptime)}} |
                     Format-Table -HideTableHeaders |
-                        Out-String).trim()
-                    
+                        Out-String).trim()             
     }
 } 
