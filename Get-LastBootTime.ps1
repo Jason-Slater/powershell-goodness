c159fb7da96b2a16bbf1347315fc686a37d1c59e
Jason Slater <jasonlslater@gmail.com>
2020-01-27 20:23:44 -0600

    Revision to fix output spacing and small tweaks

--- Get-LastBootTime.ps1
+++ Get-LastBootTime.ps1
@@ -1,12 +1,15 @@
-﻿ $Computers = Get-ADComputer -Filter { OperatingSystem -Like '*Windows Server*'}
+ $Computers = Get-ADComputer -Filter { OperatingSystem -Like '*Windows Server*'}
+
  
 foreach ($computer in $Computers) {
     
     If((Test-netconnection -ComputerName $computer.name -CommonTCPPort RDP -ErrorAction SilentlyContinue -WarningAction SilentlyContinue).TcpTestSucceeded -eq $False) {
-           Write-Host "$computer.name is no bueno" -ForegroundColor Red
-    
+           Write-Host "$computer is no bueno" -ForegroundColor Yellow   
     } ELSE {
-           Get-WmiObject -computername $computer.name win32_operatingsystem | select csname, @{LABEL='LastBootUpTime';EXPRESSION={$_.ConverttoDateTime($_.lastbootuptime)}} | ft -HideTableHeaders
-           Write-Host "=============================" -ForegroundColor Cyan
+             (Get-WmiObject -computername $computer.name win32_operatingsystem |
+                select-object csname, @{LABEL='LastBootUpTime';EXPRESSION={$_.ConverttoDateTime($_.lastbootuptime)}} |
+                    Format-Table -HideTableHeaders |
+                        Out-String).trim()
+                    
     }
 } 
