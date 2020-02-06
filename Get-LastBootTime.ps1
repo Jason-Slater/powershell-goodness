d0c0eceae38f7d79693722ec2367f97012518946
Jason Slater <jasonlslater@gmail.com>
2020-02-05 22:24:25 -0600

    Added Try/Catch

--- Get-LastBootTime.ps1
+++ Get-LastBootTime.ps1
@@ -1,12 +1,24 @@
- $Computers = Get-ADComputer -Filter { OperatingSystem -Like '*Windows Server*'}
+$Computers = (Get-ADComputer -Filter { OperatingSystem -Like '*Windows Server*'}).Name
  
-foreach ($computer in $Computers) {   
-    If((Test-netconnection -ComputerName $computer.name -CommonTCPPort RDP -ErrorAction SilentlyContinue -WarningAction SilentlyContinue).TcpTestSucceeded -eq $False) {
-           Write-Host "$computer is no bueno" -ForegroundColor Yellow   
+Foreach ($Computer in $Computers) {
+
+    TRY { $Foo = Get-WmiObject -Class Win32_OperatingSystem -Namespace root/cimv2 -ComputerName $computer -erroraction stop
+
+    } CATCH {
+
+        Write-Host "$Computer Can't check server, likely RPC server unavailable" -ForegroundColor Yellow 
+        Continue 
+        # Move to next computer
+    }
+    
+    IF((Test-netconnection -ComputerName $computer.name -CommonTCPPort RDP -ErrorAction SilentlyContinue -WarningAction SilentlyContinue).TcpTestSucceeded -eq $False)
+        { Write-Host "$Computer is no bueno" -ForegroundColor Yellow    
+
     } ELSE {
-             (Get-WmiObject -computername $computer.name win32_operatingsystem |
-                select-object csname, @{LABEL='LastBootUpTime';EXPRESSION={$_.ConverttoDateTime($_.lastbootuptime)}} |
-                    Format-Table -HideTableHeaders |
-                        Out-String).trim()             
+        (Get-WmiObject -computername $computer win32_operatingsystem |
+            select-object csname, @{LABEL='LastBootUpTime';EXPRESSION={$_.ConverttoDateTime($_.lastbootuptime)}} |
+                Format-Table -HideTableHeaders |
+                    Out-String).trim()                        
     }
 } 
+
