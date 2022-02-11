dec7d389d74c2f92a80697c7600bdac49393f6f4
Jason Slater <jasonlslater@gmail.com>
2022-02-11 08:26:31 -0600

    Minor Update

--- URL_QuickOpen.ps1
+++ URL_QuickOpen.ps1
@@ -8,3 +8,13 @@ $chrome = (Start-Process chrome.exe -Argumentlist @('-incognito',$URL,$URL2,$URL
 
 CLS
 
+
+
+#$URL1  = "https://accounts.google.com/signin/v2/identifier?service=mail&passive=1209600&osid=1&continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&followup=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&emr=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin"
+$URL2  = "https://twitter.com/login"
+$URL3  = "https://www.instagram.com/?hl=en"
+$URL4  = "https://facebook.com"
+$URL5  = "https://www.reddit.com/"
+(Start-Process firefox.exe -Argumentlist @('-incognito',$URL2,$URL3,$URL4,$URL5))
+
+CLS
