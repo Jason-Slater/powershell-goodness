7baa43abfa385585f8d1796aa4096c53a6028b74
Jason Slater <jasonlslater@gmail.com>
2022-02-11 08:30:40 -0600

    Commit All

---
+++ RiskTest.ps1
@@ -0,0 +1,14 @@
+﻿
+
+Function RiskTest
+{
+    [CmdletBinding(SupportsShouldProcess,ConfirmImpact = "High")]
+    param($ProcessName)
+
+    $p = Get-Process $ProcessName
+    if($PSCmdlet.ShouldProcess("ProcessName ($($p.Id))", "Stopping the process"))
+    {
+        Write-Host "Stopped process $ProcessName ($($p.Id))" -ForegroundColor Red
+    }
+
+}
